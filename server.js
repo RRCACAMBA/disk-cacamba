7d11e5f2040b572aed9278390e750223ad7077cf
RRCACAMBA <sthe.canal03@gmail.com>
2026-07-31 11:07:15 -0300

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');const path=require('path');const app=express();const root=__dirname;
-app.disable('x-powered-by');
-app.use((req,res,next)=>{res.set('Cache-Control',req.path==='/'?'no-cache':'public, max-age=86400');next()});
-app.use(express.static(root));
-app.get('*',(req,res)=>res.sendFile(path.join(root,'index.html')));
-const port=process.env.PORT||3000;app.listen(port,()=>console.log(`Disk Caçamba online na porta ${port}`));
