9a3bd1345997fcf7baf16d1a5c9898428a1278b8
RRCACAMBA <sthe.canal03@gmail.com>
2026-07-31 11:04:06 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.DISK_CONFIG = Object.freeze({
-  companyName: "Disk Caçamba",
-  whatsappNumber: "5511926336542",
-  whatsappDisplay: "(11) 92633-6542",
-  checkoutUrl: "https://disk-cacamba-production.up.railway.app/",
-  address: "R. Teodureto de Camargo, 28 - Vila Celeste, São Paulo - SP, 02543-130",
-  city: "São Paulo",
-  defaultMessage: "Olá! Vim pelo site da Disk Caçamba e gostaria de consultar disponibilidade e preço."
-});
